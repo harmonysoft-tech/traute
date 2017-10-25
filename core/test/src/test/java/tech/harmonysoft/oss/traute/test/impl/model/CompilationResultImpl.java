3dd8cc6cba11b71d49556a90b4dde5334ea2755c
Denis Zhdanov <dzhdanov@tango.me>
2017-10-25 15:22:30 +0300

    Enhanced CompilationResultImpl to allow to show additional info (e.g. from gradle plugin's tests)

--- core/test/src/test/java/tech/harmonysoft/oss/traute/test/impl/model/CompilationResultImpl.java
+++ core/test/src/test/java/tech/harmonysoft/oss/traute/test/impl/model/CompilationResultImpl.java
@@ -6,9 +6,15 @@ import tech.harmonysoft.oss.traute.test.api.model.CompilationResult;
 import tech.harmonysoft.oss.traute.test.api.model.TestSource;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Supplier;
 
+import static java.util.Collections.emptyMap;
+
 public class CompilationResultImpl implements CompilationResult {
+
+    private final Map<String, String> additionalInfo = new HashMap<>();
 
     @NotNull private final Supplier<Collection<ClassFile>> compiledClassesSupplier;
     @NotNull private final String compilationOutput;
@@ -18,9 +24,18 @@ public class CompilationResultImpl implements CompilationResult {
                                  @NotNull String compilationOutput,
                                  @NotNull TestSource input)
     {
+        this(compiledClassesSupplier, compilationOutput, input, emptyMap());
+    }
+
+    public CompilationResultImpl(@NotNull Supplier<Collection<ClassFile>> compiledClassesSupplier,
+                                 @NotNull String compilationOutput,
+                                 @NotNull TestSource input,
+                                 @NotNull Map<String, String> additionalInfo)
+    {
         this.compiledClassesSupplier = compiledClassesSupplier;
         this.compilationOutput = compilationOutput;
         this.input = input;
+        this.additionalInfo.putAll(additionalInfo);
     }
 
     @Override
@@ -39,5 +54,17 @@ public class CompilationResultImpl implements CompilationResult {
     @Override
     public TestSource getInput() {
         return input;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("Test source:\n\n").append(getInput()).append("\n\n");
+        buffer.append("Compile output:\n").append(getCompilationOutput());
+
+        for (Map.Entry<String, String> entry : additionalInfo.entrySet()) {
+            buffer.append("\n\n").append(entry.getKey()).append(":\n").append(entry.getValue());
+        }
+        return buffer.toString();
     }
 }
