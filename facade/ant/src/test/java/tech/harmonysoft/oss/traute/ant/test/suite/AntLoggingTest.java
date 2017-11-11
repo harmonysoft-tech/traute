7c2ff7feb6ffae221a493ed4a0f42775861a4aa8
Denis Zhdanov <dzhdanov@tango.me>
2017-11-11 09:17:08 +0300

    Issue #54: Adapt the test suite to Ant environment
    All tests are applied

---
+++ facade/ant/src/test/java/tech/harmonysoft/oss/traute/ant/test/suite/AntLoggingTest.java
@@ -0,0 +1,9 @@
+package tech.harmonysoft.oss.traute.ant.test.suite;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import tech.harmonysoft.oss.traute.ant.test.impl.TrauteAntExtension;
+import tech.harmonysoft.oss.traute.test.suite.LoggingTest;
+
+@ExtendWith(TrauteAntExtension.class)
+public class AntLoggingTest extends LoggingTest {
+}
---
+++ facade/ant/src/test/java/tech/harmonysoft/oss/traute/ant/test/suite/AntMethodReturnTest.java
@@ -0,0 +1,9 @@
+package tech.harmonysoft.oss.traute.ant.test.suite;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import tech.harmonysoft.oss.traute.ant.test.impl.TrauteAntExtension;
+import tech.harmonysoft.oss.traute.test.suite.MethodReturnTest;
+
+@ExtendWith(TrauteAntExtension.class)
+public class AntMethodReturnTest extends MethodReturnTest {
+}
