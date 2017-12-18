fe39ac79e662972eb196b24249c0e2989609fd5c
Denis Zhdanov <denzhdanov@gmail.com>
2017-12-18 16:16:45 +0300

    Issue #75: Support notNullByDefault annotations in compiled classes

--- core/javac/src/main/java/tech/harmonysoft/oss/traute/javac/common/PackageInfoManager.java
+++ core/javac/src/main/java/tech/harmonysoft/oss/traute/javac/common/PackageInfoManager.java
@@ -3,28 +3,53 @@ package tech.harmonysoft.oss.traute.javac.common;
 import com.sun.source.tree.*;
 import com.sun.source.util.TreeScanner;
 import org.jetbrains.annotations.NotNull;
+import tech.harmonysoft.oss.traute.common.util.TrauteConstants;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import javax.tools.JavaFileObject;
+import java.lang.annotation.Annotation;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
 
 import static java.util.Collections.emptySet;
 import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toSet;
 
 public class PackageInfoManager {
+
+    private static final String SUFFIX = TrauteConstants.PACKAGE_INFO + JavaFileObject.Kind.SOURCE.extension;
 
     private final ConcurrentMap<String/* package name */, Set<String> /* annotations */> packageAnnotations
             = new ConcurrentHashMap<>();
 
     @NotNull
     public Set<String> getPackageAnnotations(@NotNull String packageName) {
-        return packageAnnotations.computeIfAbsent(packageName, p -> emptySet());
+        return packageAnnotations.computeIfAbsent(packageName, p -> {
+            try {
+                Class<?> clazz = Class.forName(packageName + "." + TrauteConstants.PACKAGE_INFO);
+                Annotation[] annotations = clazz.getAnnotations();
+                if (annotations != null) {
+                    return Arrays.stream(annotations).map(a -> a.annotationType().getName()).collect(toSet());
+                }
+            } catch (ClassNotFoundException ignore) {
+            }
+            return emptySet();
+        });
     }
 
     public void onCompilationUnit(@NotNull CompilationUnitTree compilationUnit) {
+        JavaFileObject sourceFile = compilationUnit.getSourceFile();
+        if (sourceFile == null) {
+            return;
+        }
+        String name = sourceFile.getName();
+        if (name == null) {
+            return;
+        }
+        if (!name.endsWith(SUFFIX)) {
+            return;
+        }
         ExpressionTree packageNameExpression = compilationUnit.getPackageName();
         String packageName = packageNameExpression == null ? "" : packageNameExpression.toString();
         Collection<String> packageAnnotationsInSource = compilationUnit.getPackageAnnotations()
--- core/javac/src/test/java/tech/harmonysoft/oss/traute/javac/test/suite/JavacMethodParameterTest.java
+++ core/javac/src/test/java/tech/harmonysoft/oss/traute/javac/test/suite/JavacMethodParameterTest.java
@@ -1,9 +1,33 @@
 package tech.harmonysoft.oss.traute.javac.test.suite;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import tech.harmonysoft.oss.traute.javac.test.impl.TrauteJavacExtension;
+import tech.harmonysoft.oss.traute.test.impl.model.TestSourceImpl;
 import tech.harmonysoft.oss.traute.test.suite.MethodParameterTest;
+
+import static tech.harmonysoft.oss.traute.test.util.TestConstants.CLASS_NAME;
+import static tech.harmonysoft.oss.traute.test.util.TestUtil.expectNpeFromParameterCheck;
 
 @ExtendWith(TrauteJavacExtension.class)
 public class JavacMethodParameterTest extends MethodParameterTest {
+
+    @Test
+    public void notNullByDefault_compiledPackage() {
+        String myPackage = "tech.harmonysoft.oss.traute.test.fixture";
+        String testSource = String.format(
+                "package %s;\n" +
+                "" +
+                "public class %s {\n" +
+                "\n" +
+                "  public %s(Integer intParam) {\n" +
+                "  }\n" +
+                "\n" +
+                "  public static void main(String[] args) {\n" +
+                "    new %s(null);\n" +
+                "  }\n" +
+                "}", myPackage, CLASS_NAME, CLASS_NAME, CLASS_NAME);
+        expectNpeFromParameterCheck(testSource, "intParam", expectRunResult);
+        doTest(new TestSourceImpl(testSource, myPackage + "." + CLASS_NAME));
+    }
 }
